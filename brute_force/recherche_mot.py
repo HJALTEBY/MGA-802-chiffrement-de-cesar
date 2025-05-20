969119d12e105b80195bbb789a6b4cd0926c5540
Marcury1 <marcus.pinare.1@ens.etsmtl.ca>
2025-05-20 16:34:58 -0400

    Première version de la fonction pour rechercher un mot dans le fichier contenant une liste de 80000 mots

--- brute_force/recherche_mot.py
+++ brute_force/recherche_mot.py
@@ -1,4 +1,6 @@
-def chercher_mot(mot,fichier):
+chemin_dictionnaire = "../data/dict-fr-AU-DELA-common-words.ascii"
+
+def est_un_mot(mot_a_trouver,fichier):
     """
     Cherche si un mot est présent dans uun fichier. Cette fonction ignore les majuscules et les accents.
     :param mot: Le mot à chercher.
@@ -6,4 +8,14 @@ def chercher_mot(mot,fichier):
     :return: True si le mot est présent dans le fichier. False sinon.
     """
 
-    return
+    #Ouverture du fichier et chargements des mots du dictionnaire
+    with open(fichier, 'r') as file:
+        text = file.read()
+        mots = text.split()
+
+    if mots.count(mot_a_trouver) != 0:
+        return True
+    else:
+        return False
+
+print(est_un_mot('abeille',chemin_dictionnaire))
