9b3b3d8822e0056188d8b174fea3af026d18930d
Marcury1 <marcus.pinare.1@ens.etsmtl.ca>
2025-05-20 15:32:10 -0400

    Création des fichiers pour la recherche de mot pour la méthode brute force

---
+++ brute_force/__init__.py
@@ -0,0 +1,9 @@
+from .recherche_mot import *
+
+# Fonction d'aide
+def help():
+    """
+    Affiche l'aide pour l'utilisation des fonctions du package.
+    """
+    print(recherche_mot.__doc__)
+
---
+++ brute_force/recherche_mot.py
@@ -0,0 +1,9 @@
+def chercher_mot(mot,fichier):
+    """
+    Cherche si un mot est présent dans uun fichier. Cette fonction ignore les majuscules et les accents.
+    :param mot: Le mot à chercher.
+    :param fichier: Le fichier dans lequel chercher le mot.
+    :return: True si le mot est présent dans le fichier. False sinon.
+    """
+
+    return
