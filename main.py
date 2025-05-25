d3f1c1642aabc00310e83e8084255a875d42c48b
Marcury1 <marcus.pinare.1@ens.etsmtl.ca>
2025-05-25 13:01:52 -0400

    Économie d'une variable. new_cle = cle % 26 est remplacé par cle %= 26

--- main.py
+++ main.py
@@ -11,35 +11,35 @@ def decalage(cle) :
     alphabet_code = ""
     #Condition pour s'assurer que la clé est entre 26 et -26
     if cle > 26 or cle < -26 :
-        new_cle = cle % 26
-    else :
-        new_cle = cle
+        cle %= 26
+        #new_cle = cle % 26
+    #else :
+    #    new_cle = cle
     # condition si la clé est positive
-    if new_cle > 0 :
+    if cle > 0 :
         # boucle sur l'indice de liste_alphabet, où on démarre
         # à l'indice correspondant à la clé jusqu'à la fin de
         # l'alphabet
-        for i in range(0+new_cle,26) :
+        for i in range(0+cle,26) :
             alphabet_code += liste_alphabet[i]
         # boucle sur l'indice de liste_alphabet, où on démarre
         # à l'indice 0 et on va jusqu'à l'indice correspond à
         # la clé
-        for i in range(0,new_cle) :
+        for i in range(0,cle) :
             alphabet_code += liste_alphabet[i]
     # condition si la clé est négative
-    elif new_cle < 0 :
+    elif cle < 0 :
         # boucle sur l'indice de liste_alphabet, où on démarre
         # à l'indice correspondant à celui de la fin moins la clé
         # jusqu'à la fin de l'alphabet
-        for j in range((26+new_cle),26) :
+        for j in range((26+cle),26) :
             alphabet_code += liste_alphabet[j]
         # boucle sur l'indice de liste_alphabet, où on démarre
         # à l'indice 0 jusqu'à la fin de l'alphabet en enlevant
         # le nombre correspondant à la clé
-        for j in range(0,(26+new_cle)) :
+        for j in range(0,(26+cle)) :
             alphabet_code += liste_alphabet[j]
     # la clé est nulle (pas de changement)
     else :
         alphabet_code = alphabet
     return alphabet_code
-
