6a70560ffcd612abf74bac7eb7acb98e2ed45417
Marcury1 <marcus.pinare.1@ens.etsmtl.ca>
2025-05-29 20:16:23 -0400

    Changement léger dans la fonction de décryptage pour qu'elle prenne un str en entrée. Création d'une fonction de décryptage spécialement faites pour les fichiers. Test unitaire sur la fonction de décryptage.

--- cryptographie/encryption.py
+++ cryptographie/encryption.py
@@ -41,16 +41,55 @@ def encrypter(message,cle_de_cryptage):
     return contenu_crypte
 
 #décrypter dans le cas où on connait la clé
-def decrypter(cleCryptage):
-    fichierCrypte = int(str(input('Entrez le nom du fichier a decrypter (ne pas oublier le .txt): ')))
-    with open(fichierCrypte, 'r', encoding = 'utf-8') as fio :
-        # Lire le contenu du fichier
-        contenuCrypte = fio.read()
+def decrypter(message, cleCryptage):
+    """
+    Fonction permettant de décrypter un message à partir d'une clé fournie et d'un message. La fonction retourne un message
+    sans accents ni majuscules.
+    :param: Une chaine de caractère à décrypter
+    :param: Une clé de cryptage
+    :return: La chaîne de caractère décryptée
+    :raise: TypeError Si la clé entrée par l'utilisateur ne peut pas être considérée comme un entier ou si str n'est pas
+    une chaîne de caractère.
+    """
 
-    alphabetCrypte = decalage(cleCryptage)  # créer nouvel alphabet avec clé de cryptage
+    # Si l'entrée n'est pas une chaîne de caractère, on ne peut pas encrypter le message.
+    if type(message) is not str:
+        raise TypeError
+
+    # Retrait des accents et mise en minuscule
+    message = retirer_accents(message).lower()
+
+    alphabet_crypte = decalage(cleCryptage)  # créer nouvel alphabet avec clé de cryptage
+    contenu_decrypte = ''
+
+    for l in message :
+        if l.isalpha():
+            contenu_decrypte += alphabet[alphabet_crypte.find(l)]
+        else :
+            contenu_decrypte += l
+    print(f'Voici le message crypte : {contenu_decrypte}')
+
+    return contenu_decrypte
+
+def decrypter_fichier(file, cle_cryptage):
+    """
+    Fonction permettant de décrypter un message contenu dans un fichier à partir d'une clé fournie. La fonction retourne
+    un message sans accents ni majuscules.
+    :param: Le chemin vers le fichier contenant le message à décrypter
+    :param: Une clé de cryptage
+    :return: La chaîne de caractère décryptée
+    :raise: TypeError Si la clé entrée par l'utilisateur ne peut pas être considérée comme un entier ou si str n'est pas
+    une chaîne de caractère.
+    :raise: FileNotFoundError si le chemin vers le fichier n'existe pas.
+    """
+
+    with open(file, 'r', encoding='utf-8') as fio:
+        # Lire le contenu du fichier
+        contenu_crypte = fio.read()
+
+    alphabet_crypte = decalage(cle_cryptage)  # créer nouvel alphabet avec clé de cryptage
     contenuDecrypte = ''
 
-    for l in contenuCrypte :
-        contenuDecrypte[l] = alphabet[alphabetCrypte.find(l)]
+    for l in contenu_crypte:
+        contenuDecrypte[l] = alphabet[alphabet_crypte.find(l)]
     print(f'Voici le message crypte : {contenuDecrypte}')
-
