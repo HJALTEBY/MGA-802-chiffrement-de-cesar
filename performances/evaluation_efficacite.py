b3ac541c94cc869e622e85ecd1e7c0dafb36fc92
Ilona <ilona.daumas.1@ens.etsmtl.ca>
2025-05-30 04:34:47 +0200

    Script permettant de calculer le temps moyen d'exécution et renvoyant la méthode brute_force la plus rapide

--- performances/evaluation_efficacite.py
+++ performances/evaluation_efficacite.py
@@ -11,34 +11,33 @@ from cryptographie import brute_force_methode_2
 mot = 'kek'
 fichier = "../data/dict-fr-AU-DELA-common-words.ascii"
 
-# METHOODE 1
+
 # Evaluation du temps d'exécution pour l'exemple
-# Moyenne de ce temps pour 100 executions
+# Moyenne de ce temps pour un certain nombre de répétitions
 nb_repetition = 1
-total_time = 0.
+
+# METHOODE 1
+total_time1 = 0.
 for repet in range(nb_repetition):
     tic1 = perf_counter()
     brute_force_methode_1(mot,fichier)
     toc1 = perf_counter()
-    total_time += toc1-tic1
+    total_time1 += toc1-tic1
 # Affichage de la moyenne du temps d'exécution
-print(f"Le temps d'execution moyen de la première méthode est : {(total_time/nb_repetition)} [s]")
+print(f"Le temps d'execution moyen de la première méthode est : {(total_time1/nb_repetition)} [s]")
 
 
 # METHOODE 2
-# Evaluation du temps d'exécution pour l'exemple
-# Moyenne de ce temps pour 100 executions
-nb_repetition = 1
-total_time = 0.
+total_time2 = 0.
 for repet in range(nb_repetition):
     tic2 = perf_counter()
     brute_force_methode_2()
     toc2 = perf_counter()
-    total_time += toc2-tic2
+    total_time2 += toc2-tic2
 # Affichage de la moyenne du temps d'exécution
-print(f"Le temps d'execution moyen de la deuxième méthode est : {(total_time/nb_repetition)} [s]")
+print(f"Le temps d'execution moyen de la deuxième méthode est : {(total_time2/nb_repetition)} [s]")
 
-if toc2-tic2 < toc1-tic1 :
+if (toc2-tic2) < (toc1-tic1) :
     print('La deuxième méthode est plus efficace')
 else :
     print('La première méthode est plus efficace')
